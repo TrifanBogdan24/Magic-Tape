eef7c2312d627d17ec74be60e6875ec373205816
TrifanBogdan24 <trifanbogdan24@gmail.com>
2025-08-11 16:15:36 +0300

    Refactoring

--- src/tema-1.c
+++ src/tema-1.c
@@ -5,19 +5,20 @@
 #include "tema-1.h"
 
 
-void exec_operation(FILE* fout, Coada* coada, LBanda* banda, SStiva* undo, SStiva* redo) {
-    if (coada->inceput == NULL) return;
+void exec_operation(FILE* fout, Coada *coada, LBanda *banda, SStiva *undo, SStiva *redo)
+{
+    if (coada->head == NULL) return;
 
-    char cmd_to_exec[LUNGIME_LINII];
-    strcpy(cmd_to_exec, coada->inceput->informatie);
-    CoadaDeleteFirstNode(coada);
+    char cmd_to_exec[LUNGIME_LINIE];
+    strcpy(cmd_to_exec, coada->head->informatie);
+    queue_pop(coada);
 
-    if (strstr(cmd_to_exec, UPDATE_WRITE_C)) {
+    if (strstr(cmd_to_exec, WRITE_CHAR)) {
         banda->deget->caracter = cmd_to_exec[POZ_WRITE_C];
-        while (*undo) StivaPop(undo);
-        while (*redo) StivaPop(redo);
+        while (*undo) stack_pop(undo);
+        while (*redo) stack_pop(redo);
 
-    } else if (strstr(cmd_to_exec, UPDATE_MV_L_C)) {
+    } else if (strstr(cmd_to_exec, MOVE_LEFT_CHAR)) {
         NodBanda* aux = banda->deget;
         while (aux) {
             if (cmd_to_exec[POZ_MV_L_C] == aux->caracter) {
@@ -28,32 +29,32 @@ void exec_operation(FILE* fout, Coada* coada, LBanda* banda, SStiva* undo, SStiv
         }
         fprintf(fout, "%s\n", EROARE);
 
-    } else if (strstr(cmd_to_exec, UPDATE_MV_R_C)) {
+    } else if (strstr(cmd_to_exec, MOVE_RIGHT_CHAR)) {
         while (banda->deget && banda->deget->caracter != cmd_to_exec[POZ_MV_R_C]) {
             banda->deget = banda->deget->urm;
         }
         if (!banda->deget) {
-            BandaInserareFinal(banda, DIEZ);
-            banda->deget = banda->final;
+            banda_insert_to_tail(banda, DIEZ);
+            banda->deget = banda->tail;
         }
 
-    } else if (strstr(cmd_to_exec, UPDATE_INSERT_L_C)) {
-        BandaInserareStanga(fout, banda, cmd_to_exec[POZ_INSERT_L_C]);
+    } else if (strstr(cmd_to_exec, INSERT_CHAR_TO_LEFT)) {
+        banda_insert_to_left(fout, banda, cmd_to_exec[POZ_INSERT_L_C]);
 
-    } else if (strstr(cmd_to_exec, UPDATE_INSERT_R_C)) {
-        BandaInserareDreapta(banda, cmd_to_exec[POZ_INSERT_R_C]);
+    } else if (strstr(cmd_to_exec, INSERT_CHAR_TO_RIGHT)) {
+        banda_insert_to_right(banda, cmd_to_exec[POZ_INSERT_R_C]);
 
-    } else if (strstr(cmd_to_exec, UPDATE_MV_L)) {
-        if (banda->deget != banda->inceput) {
-            StivaPush(undo, banda->deget);
+    } else if (strstr(cmd_to_exec, MOVE_LEFT)) {
+        if (banda->deget != banda->head) {
+            stack_push(undo, banda->deget);
             banda->deget = banda->deget->pred;
         }
 
-    } else if (strstr(cmd_to_exec, UPDATE_MV_R)) {
-        StivaPush(undo, banda->deget);
-        if (banda->deget == banda->final) {
-            BandaInserareFinal(banda, DIEZ);
-            banda->deget = banda->final;
+    } else if (strstr(cmd_to_exec, MOVE_RIGHT)) {
+        stack_push(undo, banda->deget);
+        if (banda->deget == banda->tail) {
+            banda_insert_to_tail(banda, DIEZ);
+            banda->deget = banda->tail;
         } else {
             banda->deget = banda->deget->urm;
         }
@@ -64,63 +65,63 @@ void exec_operation(FILE* fout, Coada* coada, LBanda* banda, SStiva* undo, SStiv
 int main()
 {
     int nr = ZERO;
-    Coada* coada_execute_update = CreareCoada();    // retine numele functii
+    Coada* coada_execute_update = new_queue();    // retine numele functii
     SStiva undo_stiva = (SStiva) calloc(1, sizeof(NodStiva));               // retine numele functiei
     SStiva redo_stiva = (SStiva) calloc(1, sizeof(NodStiva));              // retine numele functiei
-    LBanda* banda_merge = CreareBanda();   // retine banda 
+    LBanda* banda_merge = new_banda();   // retine banda 
 
     FILE *fin = fopen("tema1.in" , "r");
     FILE *fout= fopen("tema1.out" , "w");
     fscanf(fin, "%d", &nr);
 
-    char  linie[LUNGIME_LINII];
+    char  linie[LUNGIME_LINIE];
     int i = ZERO;
     for (i = ZERO ; i <= nr ; i++) {
-        fgets(linie, LUNGIME_LINII , fin);
+        fgets(linie, LUNGIME_LINIE , fin);
 
-        if (strstr(linie , UPDATE_MV_R_C) != NULL) {
-            CoadaInserareFinal(coada_execute_update , linie);
-        } else if (strstr(linie ,  UPDATE_MV_L_C) != NULL) {
-            CoadaInserareFinal(coada_execute_update , linie);
-        } else if (strstr(linie , UPDATE_MV_R) != NULL) {
-            CoadaInserareFinal(coada_execute_update , linie);
-        } else if (strstr(linie , UPDATE_MV_L) != NULL) {
-            CoadaInserareFinal(coada_execute_update , linie);
-        } else if (strstr(linie , UPDATE_INSERT_L_C) != NULL) {
-            CoadaInserareFinal(coada_execute_update , linie);
-        } else if (strstr(linie , UPDATE_INSERT_R_C) != NULL) {
-            CoadaInserareFinal(coada_execute_update , linie);
+        if (strstr(linie , MOVE_RIGHT_CHAR) != NULL) {
+            queue_push(coada_execute_update , linie);
+        } else if (strstr(linie ,  MOVE_LEFT_CHAR) != NULL) {
+            queue_push(coada_execute_update , linie);
+        } else if (strstr(linie , MOVE_RIGHT) != NULL) {
+            queue_push(coada_execute_update , linie);
+        } else if (strstr(linie , MOVE_LEFT) != NULL) {
+            queue_push(coada_execute_update , linie);
+        } else if (strstr(linie , INSERT_CHAR_TO_LEFT) != NULL) {
+            queue_push(coada_execute_update , linie);
+        } else if (strstr(linie , INSERT_CHAR_TO_RIGHT) != NULL) {
+            queue_push(coada_execute_update , linie);
         } else if (strstr(linie , SHOW_CURRENT) != NULL) {
             fprintf(fout , "%c\n" , banda_merge->deget->caracter);
         } else if (strstr(linie , SHOW_ALL) != NULL) {
-            display(fout , banda_merge );
+            print_banda(fout , banda_merge);
         } else if (strstr(linie , UNDO) != NULL ) {
             if( undo_stiva != NULL && undo_stiva->informatie != NULL) {
-                StivaPush(&redo_stiva , banda_merge->deget); // punem in varful stivei REDO varful lui UNDO
+                stack_push(&redo_stiva , banda_merge->deget); // punem in varful stivei REDO varful lui UNDO
                 banda_merge->deget = undo_stiva->informatie;    // extragem pointerul din varful stivei UNDO
-                StivaPop(&undo_stiva);  // stergem varful stivei UNDO
+                stack_pop(&undo_stiva);  // stergem varful stivei UNDO
             }
         } else if (strstr(linie , REDO) != NULL ) {
             if (redo_stiva != NULL && redo_stiva->informatie != NULL) {
-                StivaPush(&undo_stiva , banda_merge->deget);     // punem in varful stivei UNDO varful lui REDO
+                stack_push(&undo_stiva , banda_merge->deget);     // punem in varful stivei UNDO varful lui REDO
                 banda_merge->deget = redo_stiva->informatie;  // extragem pointerul din varful stivei REDO
-                StivaPop(&redo_stiva);  // stergem varful stivei REDO
+                stack_pop(&redo_stiva);  // stergem varful stivei REDO
             }
-        } else if( strstr(linie , UPDATE_WRITE_C) != NULL) {
-            CoadaInserareFinal( coada_execute_update , linie);
+        } else if( strstr(linie , WRITE_CHAR) != NULL) {
+            queue_push( coada_execute_update , linie);
         } else if (strstr(linie , EXECUTE) != NULL) {
             exec_operation(fout, coada_execute_update, banda_merge, &undo_stiva, &redo_stiva);
         }
     }
 
-    BandaStergere(banda_merge);
-    CoadaFullDelete(coada_execute_update);
+    delete_banda(banda_merge);
+    delete_queue(coada_execute_update);
 
     while (undo_stiva != NULL) {
-        StivaPop(&undo_stiva);  
+        stack_pop(&undo_stiva);  
     }
     while (redo_stiva!= NULL) {
-        StivaPop(&redo_stiva);
+        stack_pop(&redo_stiva);
     }
 
     free(undo_stiva);
--- src/tema-1.h
+++ src/tema-1.h
@@ -1,24 +1,29 @@
 /*TRIFAN BOGDAN-CRISTIAN , 312CD*/
 #ifndef TEMA_1_H
 #define TEMA_1_H
-#include <stdio.h>                                          // needed for printf
+#include <stdio.h>                            // needed for printf
 #include <stdlib.h>                           // needed for alloc, realloc, free
-#define UPDATE_MV_L "MOVE_LEFT"
-#define UPDATE_MV_R "MOVE_RIGHT"
-#define UPDATE_MV_L_C "MOVE_LEFT_CHAR"
-#define UPDATE_MV_R_C "MOVE_RIGHT_CHAR"
-#define UPDATE_WRITE_C "WRITE"
-#define UPDATE_INSERT_L_C "INSERT_LEFT"
-#define UPDATE_INSERT_R_C "INSERT_RIGHT"
+
+// UPDATE operations:
+#define MOVE_LEFT "MOVE_LEFT"
+#define MOVE_RIGHT "MOVE_RIGHT"
+#define MOVE_LEFT_CHAR "MOVE_LEFT_CHAR"
+#define MOVE_RIGHT_CHAR "MOVE_RIGHT_CHAR"
+#define WRITE_CHAR "WRITE"
+#define INSERT_CHAR_TO_LEFT "INSERT_LEFT"
+#define INSERT_CHAR_TO_RIGHT "INSERT_RIGHT"
+
+// QUERY operations:
 #define SHOW_CURRENT "SHOW_CURRENT"
 #define SHOW_ALL "SHOW"
 #define EXECUTE "EXECUTE"
 #define UNDO "UNDO"
-#define REDO "REDO" 
+#define REDO "REDO"
+
 #define DIEZ '#'
 #define SPACE ' '
 #define EROARE "ERROR"
-#define LUNGIME_LINII 20
+#define LUNGIME_LINIE 20
 #define POZ_WRITE_C 6
 #define POZ_MV_L_C 15
 #define POZ_MV_R_C 16
@@ -32,272 +37,257 @@ int k = ZERO;
 
 typedef struct NodBanda {
     char caracter;
-    struct NodBanda* urm;    // adresa catre urmatorul nod din Coada
-    struct NodBanda* pred;   // adresa catre nodul anterior din Coada
+    struct NodBanda *urm;    // adresa catre urmatorul nod din Coada
+    struct NodBanda *pred;   // adresa catre nodul anterior din Coada
 } NodBanda;
 
 
 typedef struct LBanda{
-    NodBanda* inceput;       // va memora nodul de inceput al listei
-    NodBanda* final;         // va memora nodul final al listei
-    NodBanda* santinela;    // santinela, fundatia listei
-    NodBanda* deget;         // va memora nodul catre care degetul indica
+    NodBanda *head;       // va memora nodul de inceput al listei
+    NodBanda *tail;         // va memora nodul final al listei
+    NodBanda *santinela;    // santinela, fundatia listei
+    NodBanda *deget;         // va memora nodul catre care degetul indica
 } LBanda;
 
 
 typedef struct Stiva {
-    struct Stiva* urm;
-    NodBanda* informatie;
+    struct Stiva *urm;
+    NodBanda *informatie;
 } NodStiva, *SStiva;
 
 
 typedef struct NodCoada {
-    char informatie[LUNGIME_LINII];
+    char informatie[LUNGIME_LINIE];
     struct NodCoada* urm;    // adresa catre urmatorul nod din Coada
     struct NodCoada* pred;   // adresa catre nodul anterior din Coada
 } NodCoada;
 
 
 typedef struct Coada {
-    NodCoada* inceput;       // va memora nodul de inceput al listei
-    NodCoada* final;         // va memora nodul final al listei
-
+    NodCoada* head;       // va memora nodul de head al listei
+    NodCoada* tail;         // va memora nodul tail al listei
 } Coada;
 
 
 
-NodCoada *CreareNodCoada(char informatie[LUNGIME_LINII]) {
+NodCoada *new_queue_node(char informatie[LUNGIME_LINIE])
+{
     // cream un nou nod
-    NodCoada* nod_nou = malloc( sizeof(NodCoada));
-    nod_nou -> urm = nod_nou -> pred = NULL;
-    strcpy(nod_nou -> informatie, informatie);
-   return nod_nou;
+    NodCoada* new_node = malloc(sizeof(NodCoada));
+    new_node->urm = new_node->pred = NULL;
+    strcpy(new_node->informatie, informatie);
+   return new_node;
 }
 
 
-NodBanda *CreareNodBanda(char caracter) {
+NodBanda *new_banda_node(char caracter)
+{
     // cream un nou nod
-    NodBanda* nod_nou = malloc(sizeof(NodBanda));
-   
-    nod_nou -> urm = nod_nou -> pred = NULL;
-    nod_nou -> caracter = caracter;
-   return nod_nou;
+    NodBanda* new_node = malloc(sizeof(NodBanda));
+    new_node->urm = new_node->pred = NULL;
+    new_node->caracter = caracter;
+    return new_node;
 }
 
 
-LBanda* CreareBanda(void) {
+LBanda* new_banda()
+{
     // creem o banda cu santinela
-    LBanda* banda_noua = malloc(sizeof(LBanda));
-    banda_noua->inceput = banda_noua->final = banda_noua->deget = CreareNodBanda(DIEZ);
-    banda_noua->santinela = CreareNodBanda(SPACE);
-    banda_noua->santinela->urm = banda_noua->inceput;
-    banda_noua->inceput->pred = banda_noua->santinela;
+    LBanda *banda_noua = malloc(sizeof(LBanda));
+    banda_noua->head = banda_noua->tail = banda_noua->deget = new_banda_node(DIEZ);
+    banda_noua->santinela = new_banda_node(SPACE);
+    banda_noua->santinela->urm = banda_noua->head;
+    banda_noua->head->pred = banda_noua->santinela;
     return banda_noua;
 }
 
 
-void BandaStergere(LBanda* banda_input) {
-    while (banda_input->final != banda_input->santinela) {
-        NodBanda* stergator = banda_input->final;
-        banda_input->final = banda_input->final->pred;
-        free(stergator);
+void delete_banda(LBanda *banda)
+{
+    while (banda->tail != banda->santinela) {
+        NodBanda* tmp = banda->tail;
+        banda->tail = banda->tail->pred;
+        free(tmp);
     }
-    free(banda_input->santinela);
-    free(banda_input);
+    free(banda->santinela);
+    free(banda);
 }
 
 
-void BandaInserareFinal(LBanda* banda_input ,char caracter) {
-    
-    if (banda_input == NULL || banda_input->inceput == NULL) {
-        // Coada vida , elementul va deveni primul nod al listei
-        banda_input -> inceput = banda_input -> final = CreareNodBanda(caracter);
-        banda_input->santinela->urm = banda_input->inceput;
-        banda_input->inceput->pred = banda_input->santinela;
+void banda_insert_to_tail(LBanda *banda, char caracter)
+{
+    if (!banda || !banda->head) {
+        // Coada vida, elementul va deveni primul nod al listei
+        banda->head = banda->tail = new_banda_node(caracter);
+        banda->santinela->urm = banda->head;
+        banda->head->pred = banda->santinela;
         return;
     }
     
     // Coada  cel putin un element
-    NodBanda* nodul_final = CreareNodBanda(caracter);
-    nodul_final -> pred = banda_input -> final;
-    banda_input -> final -> urm = nodul_final;
-    banda_input -> final = nodul_final;
-
-    return ;
-   
+    NodBanda* nodul_final = new_banda_node(caracter);
+    nodul_final->pred = banda->tail;
+    banda->tail-> urm = nodul_final;
+    banda->tail = nodul_final;
 }
 
 
-void BandaInserareStanga(FILE* fout , LBanda *banda_input , char caracter) {
+void banda_insert_to_left(FILE *fout, LBanda *banda, char caracter)
+{
     // inseram in stanga santinelei
-    if (banda_input->deget == banda_input->inceput) {
-        // inceput de banda
-
-        fprintf(fout, "%s\n" , EROARE);
+    if (banda->deget == banda->head) {
+        // head de banda
+        fprintf(fout, "%s\n", EROARE);
         return;
     } else {
-        NodBanda* nod_inserare_st = CreareNodBanda(caracter);
-        nod_inserare_st->pred = banda_input->deget->pred;
-        nod_inserare_st->urm = banda_input->deget;
-        banda_input->deget->pred->urm = nod_inserare_st;
-        banda_input->deget->pred = nod_inserare_st;
-        banda_input->deget = nod_inserare_st;
+        NodBanda* nod_inserare_st = new_banda_node(caracter);
+        nod_inserare_st->pred = banda->deget->pred;
+        nod_inserare_st->urm = banda->deget;
+        banda->deget->pred->urm = nod_inserare_st;
+        banda->deget->pred = nod_inserare_st;
+        banda->deget = nod_inserare_st;
         return;
     }
 }
 
 
-void BandaInserareDreapta(LBanda *banda_input , char caracter) {
+void banda_insert_to_right(LBanda *banda, char caracter)
+{
     // inseram in dreapta santinelei
-    if (banda_input->deget == banda_input->final) {
-        // inseram la final
-        BandaInserareFinal(banda_input , caracter);
-        banda_input->deget = banda_input->final; 
-        // printf("%s\n" , banda_input->deget->informatie);
+    if (banda->deget == banda->tail) {
+        // inseram la final de banda
+        banda_insert_to_tail(banda , caracter);
+        banda->deget = banda->tail; 
         return;
     } else {
-        NodBanda* nod_inserare_dr = CreareNodBanda(caracter);
-        nod_inserare_dr->pred = banda_input->deget;
-        nod_inserare_dr->urm = banda_input->deget->urm;
-        banda_input->deget->urm->pred = nod_inserare_dr;
-        banda_input->deget->urm = nod_inserare_dr;
-        banda_input->deget = nod_inserare_dr;
+        NodBanda* new_node = new_banda_node(caracter);
+        new_node->pred = banda->deget;
+        new_node->urm = banda->deget->urm;
+        banda->deget->urm->pred = new_node;
+        banda->deget->urm = new_node;
+        banda->deget = new_node;
         return;
     }
 }
 
 
-void display(FILE* fout , LBanda *banda_input) {
-    NodBanda* parcurgere = banda_input -> inceput;
-    while (parcurgere != NULL) {
-        if (parcurgere == banda_input -> deget) {
-            fprintf(fout , "|%c|" , parcurgere -> caracter);
+void print_banda(FILE *fout, LBanda *banda)
+{
+    for (NodBanda* iter = banda->head; iter; iter = iter->urm) {
+        if (iter == banda->deget) {
+            fprintf(fout , "|%c|" , iter->caracter);
         } else {
-            fprintf(fout , "%c" , parcurgere -> caracter);
+            fprintf(fout , "%c" , iter->caracter);
         }
-        parcurgere = parcurgere -> urm;
     }
-    fprintf(fout , "\n");
-    return ;
+    fprintf(fout, "\n");
 }
 
 
 
-Coada* CreareCoada(void) {
-
+Coada* new_queue()
+{
 	// cream o Coada noua
 	Coada* coada_noua = malloc(sizeof(Coada));
-	coada_noua -> inceput = NULL;
-	coada_noua -> final = NULL;
+	coada_noua->head = NULL;
+	coada_noua->tail = NULL;
 	return coada_noua;
 }
 
-void CoadaInserareInceput(Coada* coada_input, char informatie[LUNGIME_LINII]) {
 
-    // Coada are cel putin un element
-    NodCoada* new_nod = CreareNodCoada(informatie);
-    new_nod->urm = coada_input->inceput;
-    if (coada_input->inceput != NULL) {
-        coada_input->inceput->pred = new_nod;
-    }
-    coada_input->inceput = new_nod;
-    return ;
-}
-
-void CoadaInserareFinal(Coada* coada_input, char informatie[LUNGIME_LINII]) {
-    if (coada_input == NULL) {
+void queue_push(Coada* queue, char informatie[LUNGIME_LINIE])
+{
+    if (!queue) {
         // Coada este vida
-        coada_input = CreareCoada();
-        coada_input->inceput =  coada_input->final = CreareNodCoada(informatie);
+        queue = new_queue();
+        queue->head =  queue->tail = new_queue_node(informatie);
         return;
     } 
-    if (coada_input != NULL && coada_input->inceput == NULL) {
+    if (queue && !queue->head) {
         // Coada nu este initializata, ea exista, dar nu retine nimic
-        coada_input->inceput = coada_input->final = CreareNodCoada(informatie);
+        queue->head = queue->tail = new_queue_node(informatie);
         return;
     }
     // Coada are cel putin un element
-    NodCoada* new_nod = CreareNodCoada(informatie);
-    new_nod->pred = coada_input->final;
-    coada_input->final->urm = new_nod;
-    coada_input->final = new_nod;
-    return ;
+    NodCoada* new_nod = new_queue_node(informatie);
+    new_nod->pred = queue->tail;
+    queue->tail->urm = new_nod;
+    queue->tail = new_nod;
 }
 
 
-void CoadaDeleteFirstNode(Coada* coada_input) {
+void queue_pop(Coada *queue)
+{
     // sterge orimul element al listei , dar nu si Coada
-    NodCoada* stergator = coada_input -> inceput;
-    coada_input -> inceput = coada_input -> inceput -> urm;
-    if (coada_input -> inceput != NULL) {
-        coada_input -> inceput -> pred = NULL;
+    NodCoada* stergator = queue->head;
+    queue->head = queue->head->urm;
+    if (queue->head) {
+        queue->head->pred = NULL;
     }
     free(stergator);
-    return ;
-
 }
 
 
-void CoadaDeleteLastnod(Coada* coada_input) {
-    if (coada_input == NULL) return;
-    if (coada_input->inceput == coada_input->final) {
+void CoadaDeleteLastnod(Coada *queue)
+{
+    if (!queue) return;
+    if (queue->head == queue->tail) {
         // Coada are un singur element
-        // coada_input -> inceput = coada_input -> final = NULL;
-        free(coada_input->inceput);
-        free(coada_input->final);
+        free(queue->head);
+        free(queue->tail);
         return;
     } else {
         // Coada are mai multe elemente
-        coada_input->final = coada_input->final->pred;
-        free(coada_input->final->urm);
-        return ;
+        queue->tail = queue->tail->pred;
+        free(queue->tail->urm);
+        return;
     }
 }
 
 
-void CoadaFullDelete(Coada* coada_input){
+void delete_queue(Coada *queue)
+{
 	// Guard against young player errors
-	if (coada_input == NULL) {
-        free(coada_input);
-        return ;
+	if (!queue) {
+        free(queue);
+        return;
     }
-	while (coada_input -> inceput != NULL) {
+	while (queue->head) {
 		// stergem succeiv primul element al listei
-		// o privim practic asemenea unei cozis
+		// o privim practic asemenea unei cozi
         // stergere de la inceput
-		CoadaDeleteFirstNode(coada_input);
+		queue_pop(queue);
 	}
-	free(coada_input);
-	return ;
+	free(queue);
 }
 
 
 
-void StivaPush(SStiva *stiva_input, NodBanda* varf_nou) {
+void stack_push(SStiva *stiva, NodBanda *varf_nou)
+{
     SStiva stiva_noua = (SStiva) malloc(sizeof(NodStiva));
-    stiva_noua -> informatie = varf_nou;
-    stiva_noua->urm = *stiva_input;
-    *stiva_input = stiva_noua;
-    return ; 
-}
-    
-
-void StivaPop(SStiva* stiva_input) {
-    if (stiva_input != NULL) {
-        SStiva stergator = *stiva_input;
-        *stiva_input = (*stiva_input)->urm;
-        free(stergator);
-    }
-
+    stiva_noua->informatie = varf_nou;
+    stiva_noua->urm = *stiva;
+    *stiva = stiva_noua;
 }
 
 
-void DeleteStiva(SStiva* stiva_input) {
-    while (*stiva_input != NULL) {
-        StivaPop(stiva_input);
+void stack_pop(SStiva *stiva)
+{
+    if (stiva != NULL) {
+        SStiva tmp = *stiva;
+        *stiva = (*stiva)->urm;
+        free(tmp);
     }
-    free(stiva_input);
-    return;
+}
+
+
+void delete_stack(SStiva *stiva)
+{
+    while (*stiva != NULL) {
+        stack_pop(stiva);
+    }
+    free(stiva);
 }
 
 #endif
